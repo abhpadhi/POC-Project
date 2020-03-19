8c56bb09d6bc53f0f7bce31f88280a514d1fcf8f
Abhinab Padhi <abhpadhi@publicissapient.com>
2020-03-19 12:41:33 +0530

    second commit

--- Lambda.tf
+++ Lambda.tf
@@ -72,12 +72,19 @@ resource "aws_cloudwatch_event_rule" "trigger_lambda" {
   schedule_expression = "rate(5 minutes)"
 }
 
+resource "aws_cloudwatch_event_target" "run_lambda_five_minutes"{
+  rule = aws_cloudwatch_event_rule.trigger_lambda.name
+  target_id = "lambda"
+  arn = "aws_lambda_function.project.arn"
+}
+
+
 #AWS 2nd Lambda Creation 
-#resource "aws_lambda_function" "project-API" {
-#    function_name = "project_syncron-API"
-#    filename      = "code_for_second_lambda_function.zip"
-#    role          = aws_iam_role.iam_for_lambda.arn
-#    handler       = "lambda_handler"
-#    source_code_hash = filebase64sha256("code_for_second_lambda_function.zip")  
-#    runtime = "python3.7" 
-#}
+resource "aws_lambda_function" "project-API" {
+    function_name = "project_syncron-API"
+    filename      = "code_for_second_lambda_function.zip"
+    role          = aws_iam_role.iam_for_lambda.arn
+    handler       = "lambda_handler"
+    source_code_hash = filebase64sha256("code_for_second_lambda_function.zip")  
+    runtime = "python3.7" 
+}
