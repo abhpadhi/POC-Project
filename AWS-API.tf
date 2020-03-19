21d22b1997e461a6f74608daaa358dc82435abb3
Abhinab Padhi <abhpadhi@publicissapient.com>
2020-03-19 18:48:31 +0530

    fourth commit

---
+++ AWS-API.tf
@@ -0,0 +1,30 @@
+resource "aws_api_gateway_rest_api" "API_Project" {
+    name = "API-RDS-Info"
+    description = "API gateway to fetch RDS instance details from DynamobDB"
+    api_key_source = "HEADER"
+    endpoint_configuration {
+        types = ["EDGE OPTIMIZED"]
+    }
+}
+
+resource "aws_api_gateway_resource" "api_resource" {
+  rest_api_id = aws_api_gateway_rest_api.API_project.id
+  parent_id   = aws_api_gateway_rest_api.API_project.root_resource_id
+  path_part   = "rdsinfo"
+}
+
+resource "aws_api_gateway_method" "getmethod" {
+  rest_api_id   = aws_api_gateway_rest-api.API_Project.id 
+  resource_id   = aws_api+gateway_resource.api_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "api_integration" {
+  rest_api_id       = aws_api_gateway_rest_api.API_Project.id  
+  resource_id       = aws_api_gateway_resource.api_resource.id  
+  http_method       = aws_api_gateway_method.getmethod.http_method  
+  type              = "AWS"
+  uri               =  ""
+}
+
