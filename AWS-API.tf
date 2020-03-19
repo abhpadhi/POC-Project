2dafa90c6572967f86f3b536bf2d69d0dddf44e0
Abhinab Padhi <abhpadhi@publicissapient.com>
2020-03-19 20:04:49 +0530

    Fifth commit

--- AWS-API.tf
+++ AWS-API.tf
@@ -18,13 +18,34 @@ resource "aws_api_gateway_method" "getmethod" {
   resource_id   = aws_api+gateway_resource.api_resource.id
   http_method   = "GET"
   authorization = "NONE"
+  request_parameters = { 
+	    "integration.request.querystring.var1"="'InstanceType'"
+  }
 }
 
+#resource "aws_api_gateway_method" "gateway_method" {
+#
+#  
+#  
+#  request_parameters = { 
+#	    "integration.request.querystring.var1"="'InstanceType'"
+#  }
+#}
+
+
 resource "aws_api_gateway_integration" "api_integration" {
-  rest_api_id       = aws_api_gateway_rest_api.API_Project.id  
-  resource_id       = aws_api_gateway_resource.api_resource.id  
-  http_method       = aws_api_gateway_method.getmethod.http_method  
-  type              = "AWS"
-  uri               =  ""
+  rest_api_id             = aws_api_gateway_rest_api.API_Project.id  
+  resource_id             = aws_api_gateway_resource.api_resource.id  
+  http_method             = aws_api_gateway_method.getmethod.http_method  
+  type                    = "AWS"
+  integration_http_method = "GET"
+  uri                     = aws_lambda_function.project.invoke_arn
+  request_templates = {
+    "application/json" = <<EOF
+      {
+        "dbType": "$input.params('InstanceType')"
+      }
+  }
+
 }
 
