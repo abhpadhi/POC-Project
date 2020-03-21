4dce3480ee4d27a95b1c4d489fd1d9a6197e1b5b
Abhinab Padhi <abhpadhi@publicissapient.com>
2020-03-21 16:32:06 +0530

    v18

--- provider.tf
+++ provider.tf
@@ -1,9 +1,11 @@
 #AWS Provider
 provider "aws" {
     region     = "us-east-1"
-    access_key = "AKIAY674NBQD7JGH2FER"
-    secret_key = "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" 
+    #access_key = "AKIAY674NBQD7JGH2FER"
+    #secret_key = "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" 
+    shared_credentials_file = "~/.aws/credentials"
     vesion     = "~> 2.9.0"
+    profile  =  "default"
 }
 
 #terraform {
