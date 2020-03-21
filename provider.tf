03e4745af13c4ff0d672348295840396b2707fb0
Abhinab Padhi <abhpadhi@publicissapient.com>
2020-03-21 18:37:50 +0530

    v18

--- provider.tf
+++ provider.tf
@@ -2,10 +2,5 @@
 provider "aws" {
     region     = "us-east-1"
     shared_credentials_file = "~/.aws/credentials"
-    version     = "~> 2.53.0"
     profile  =  "default"
 }
-
-#terraform {
-#  required_version = ">= 0.11.8"
-#}
