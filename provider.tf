e06182835db7e8b2f46904f3a7d49845933c8ca6
Abhinab Padhi <abhpadhi@publicissapient.com>
2020-03-21 16:34:09 +0530

    v18

--- provider.tf
+++ provider.tf
@@ -1,10 +1,8 @@
 #AWS Provider
 provider "aws" {
     region     = "us-east-1"
-    #access_key = "AKIAY674NBQD7JGH2FER"
-    #secret_key = "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" 
-    shared_credentials_file = "~/.aws/credentials"
-    vesion     = "~> 2.9.0"
+    shared_credentials_file = "~/.aws/credentials"
+    version     = "~> 2.53.0"
     profile  =  "default"
 }
 
